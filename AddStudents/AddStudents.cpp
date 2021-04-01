ee8840796ee5a8cbb5756eb4edd39208904c8e4c
Андрей Наумов <naumov.net@gmail.com>
2021-04-01 23:37:17 +0300

    Revert "new-comit-c_plus"
    This reverts commit ad800c24378a3c6013c41ad71772ab275d97bc44.

--- AddStudents/AddStudents.cpp
+++ AddStudents/AddStudents.cpp
@@ -7,7 +7,6 @@
 #include <cstring>
 #include <iomanip>
 #include <limits>
-#include <string>
 
 
 using namespace std;
@@ -15,20 +14,19 @@ using namespace std;
 struct students {
     char fio[100];
     char sex[100];
-    int groupNumber;
+    double groupNumber;
     int groupListNumber;
-    int gradesForTheLastSession[8];
+    double gradesForTheLastSession;
     char formOfEducation[100];
     time_t dataChanges;
 };
 
 int Size;
 
-// Создание новой записи о студенте
 void createStudents () {
 
     students stu;
-    string path = "test_2.txt";
+    string path = "test_3.txt";
     fstream fs;
     fs.open(path, fstream::in | fstream::out | fstream::app);
 
@@ -49,16 +47,12 @@ void createStudents () {
         cin >> stu.groupListNumber;
 
         cout << "\nEnter the Grades For The Last Session: ";
-
-        for (int i = 0; i < 8; i++) {
-            cin >> stu.gradesForTheLastSession[i];
-        }
+        cin >> stu.gradesForTheLastSession;
 
         cout << "\nEnter the Form Of Education: ";
         cin >> stu.formOfEducation;
 
         stu.dataChanges = time (NULL);
-        cout << "\n";
 
         cout << "FIO: " << stu.fio << "\n";
         cout << "SEX: " << stu.sex << "\n";
@@ -72,12 +66,7 @@ void createStudents () {
         fs << stu.sex << '\n';
         fs << stu.groupNumber << '\n';
         fs << stu.groupListNumber << '\n';
-
-        for (int i = 0; i < 8; i++) {
-            fs << stu.gradesForTheLastSession[i] << " ";
-        }
-        fs << "\n";
-
+        fs << stu.gradesForTheLastSession << '\n';
         fs << stu.formOfEducation << '\n';
         fs << stu.dataChanges << '\n';
     }
@@ -88,7 +77,7 @@ void createStudents () {
 void makingChangesToAnexiStingRecord() {
 
     students stu;
-    string path = "test_2.txt";
+    string path = "test_3.txt";
     fstream fs;
     fs.open(path, fstream::in | fstream::out | fstream::app);
 
@@ -108,9 +97,7 @@ void makingChangesToAnexiStingRecord() {
         cin >> stu.groupListNumber;
 
         cout << "\nEdit the Grades For The Last Session: ";
-        for (int i = 0; i < 8; i++) {
-            cin >> stu.gradesForTheLastSession[i];
-        }
+        cin >> stu.gradesForTheLastSession;
 
         cout << "\nEdit the Form Of Education: ";
         cin >> stu.formOfEducation;
@@ -136,11 +123,9 @@ void makingChangesToAnexiStingRecord() {
 }
 
 // функция возврат заначения
-students * getAllDataFromFile() {
-
-    students* List = NULL; // 
+students* getAllDataFromFile() {
+    students* List = NULL;
     ifstream file;
-
     file.open("test_2.txt");
     while (!file.is_open()) {
 
@@ -150,7 +135,7 @@ students * getAllDataFromFile() {
 
     Size = 0;
 
-    while (!file.eof()) {
+    while (Size != 3) {
         List = (students*) realloc(List, (Size + 1) * sizeof(students));
         students* newStudent = List + Size;
         file.getline(newStudent->fio, 100);
@@ -158,12 +143,8 @@ students * getAllDataFromFile() {
         file >> newStudent->groupNumber;
         file.ignore(numeric_limits<streamsize>::max(), '\n');
         file >> newStudent->groupListNumber;
-
         file.ignore(numeric_limits<streamsize>::max(), '\n');
-        for (int i = 0; i < 8; i++) {
-            file >> newStudent->gradesForTheLastSession[i];
-        }
-
+        file >> newStudent->gradesForTheLastSession;
         file.ignore(numeric_limits<streamsize>::max(), '\n');
         file.getline(newStudent->formOfEducation, 100);
         file >> newStudent->dataChanges;
@@ -172,10 +153,9 @@ students * getAllDataFromFile() {
     }
 
     file.close();
-    return List; // возращает указатель впамати
+    return List;
 }
 
-// маска для вывода полей
 void printStudents(students* stu) {
     
     for (int i = 0; i < Size; i++) { 
@@ -184,19 +164,14 @@ void printStudents(students* stu) {
         cout << "SEX: " << Current->sex << "\n";
         cout << "Group Number: " << Current->groupNumber << "\n";
         cout << "Group List Number: " << Current->groupListNumber << "\n";
-        cout << "Grades For The Last Session: " << " ";
-        for (int i = 0; i < 8; i++) {
-            cout << Current->gradesForTheLastSession[i] << " ";
-        }
-        cout << "\n";
-
+        cout << "Grades For The Last Session: " << Current->gradesForTheLastSession << "\n";
         cout << "The Form Of Education: " << Current->formOfEducation << "\n";
         cout << "The Data Changes: " << ctime(&(Current->dataChanges)) << "\n";
     }
     stu = stu + 1;
 }
 
-// функция изменения записи
+
 void editPrintStudents(students* stu) {
     
     ofstream file;
@@ -209,19 +184,13 @@ void editPrintStudents(students* stu) {
     } else {
 
         for (int i = 0; i < Size; i++) { 
-
             students* Current = stu + i;
 
             file << Current->fio << '\n';
             file << Current->sex << '\n';
             file << Current->groupNumber << '\n';
             file << Current->groupListNumber << '\n';
-
-            for (int i = 0; i < 8; i++) {
-                file << Current->gradesForTheLastSession[i] << " ";
-            }  
-            file << "\n";
-
+            file << Current->gradesForTheLastSession << '\n';
             file << Current->formOfEducation << '\n';
             file << Current->dataChanges;
             //file << ctime(&(Current->dataChanges));
@@ -235,7 +204,6 @@ void editPrintStudents(students* stu) {
 
 }
 
-// функция записывает в память
 void memoryEdit(students* stu) {
 
     students* Current;
@@ -251,140 +219,107 @@ void memoryEdit(students* stu) {
     cin >> number;
     Current = stu + number - 1;
 
-    cout << "FIO: ";
-    cin >> Current->fio;
-    cout << "SEX: ";
-    cin >> Current->sex;
-    cout << "Group Number: ";
-    cin >> Current->groupNumber;
-    cout << "Group List Number: ";
-    cin >> Current->groupListNumber;
+    //cout << "FIO: ";
+    //cin >> Current->fio;
+    //cout << "SEX: ";
+    //cin >> Current->sex;
+    //cout << "Group Number: ";
+    //cin >> Current->groupNumber;
+    //cout << "Group List Number: ";
+    //cin >> Current->groupListNumber;
     cout << "Grades For The Last Session: ";
+    cin >> Current->gradesForTheLastSession;
+    //cout << "The Form Of Education: ";
+    //cin >> Current->formOfEducation;
+    //cout << "The Data Changes: ";
 
-    for (int i = 0; i < 8; i++) {
-        cin >> Current->gradesForTheLastSession[i];
-    }
-    
-    cout << "The Form Of Education: ";
-    cin >> Current->formOfEducation;
-    cout << "The Data Changes: ";
-
-    cin.getline(Current->sex, 100);
+    //cin.getline(Current->sex, 100);
 
     editPrintStudents(stu);
 }
 
+//void outputallstudentsgroupn () {
+//
+//    students stu;
+//    ifstream file;
+//    file.open("test_3.txt");
+//
+//    char buff[1024];
+//    int i = 0;
+//
+//    while (!file.eof()) {
+//
+//        if (stu.gradesforthelastsession < 4) {
+//
+//            file.getline(buff, sizeof(buff));
+//            cout << buff << "\n";
+//
+//            cout << "fio: " << stu.fio;
+//            cout << "\ngrades for the last session: " << stu.gradesforthelastsession;
+//        }
+//
+//        else {
+//            cout << stu.fio;
+//            cout << "\n error";
+//            cout << stu.gradesforthelastsession;
+//        }
+//    }
+//
+//    file.close();
+//    cin.ignore();
+//}
+
+void listStu(students * stu) {
+
+    students* Current;
+
+    for (int i = 0; i < Size; i++) {
+        Current = stu + i;
+        cout << i + 1 << ". " << Current->groupNumber<< '\n';
+    }
+
+    cout << "Enter the group: ";
+    int number;
+    cin >> number;
+    Current = stu + number - 1;
+    cout << endl;
+
+    //cin >> Current->groupNumber;
+
+    if (number) {
+        printStudents(stu);
+    }
+
+    else {
+        cout << "Error!";
+    }
+
+    
+
+    //cin >> Current->gradesForTheLastSession;
+}
+
 //Вывод информации обо всех студентах группы N. N – инициализируется пользователем
 
-void outputAllStudentsGroupN(students* List) {
+//void outputAllStudentsGroupN(students*stu) {
+//     
+//}
 
-    int n;
-    cout << "Enter the group number:" << endl;
-    cin >> n;
-
-    for (int i = 0; i < Size; i++) {
-
-        if (List[i].groupNumber == n) {
-
-            cout << List[i].fio << endl;
-            cout << List[i].sex << endl;
-            cout << List[i].groupNumber << endl;
-            cout << List[i].groupListNumber << endl;
-
-            cout << "Term grades:";
-            for (int j = 0; j < 8; j++) {
-                cout << List[i].gradesForTheLastSession[j] << " ";
-
-            }
-            cout << endl;
-
-            cout << List[i].formOfEducation << endl;
-            cout << ctime(&(List[i].dataChanges)) << endl;
-        }
-    }
-}
-
-void listTop(students* List)
-{
-    cout << "Top students:" << endl << endl;
-    // структура для хранения средних балов
-    struct sortList
-    {
-        unsigned int number;
-        double score;
-    };
-
-    sortList* SortList = (sortList*) malloc(sizeof(sortList) * Size);
-
-    for (int i = 0; i < Size; i++) {
-
-        int sum = 0;
-        (SortList + i)->number = i;
-
-        for (int j = 0; j < 8; j++) {
-            sum = (sum + List[i].gradesForTheLastSession[j]);
-            (SortList + i)->score = (double)sum / 8;
-        }
-    }
-
-    int sorted = 0;
-    int currentMaxUnsorted = Size - 1;
-    while (!sorted)
-    {
-        sorted = 1;
-        for (int i = 0; i < currentMaxUnsorted; i++)
-        {
-            if ((SortList + i)->score - (SortList + i + 1)->score > 0)
-            {
-                sortList Buffer;
-                Buffer.number = (SortList + i)->number;
-                Buffer.score = (SortList + i)->score;
-                (SortList + i)->number = (SortList + i + 1)->number;
-                (SortList + i)->score = (SortList + i + 1)->score;
-                (SortList + i + 1)->number = Buffer.number;
-                (SortList + i + 1)->score = Buffer.score;
-                sorted = 0;
-            }
-        }
-        currentMaxUnsorted--; // последний уже отсортирован и проверсять не надо 
-    }
-
-    for (unsigned int i = Size - 1, j = 0; i >= 0 && j < 3; i--, j++)
-    {
-        cout << List[(SortList + i)->number].fio << endl;
-        cout << List[(SortList + i)->number].sex << endl;
-        cout << List[(SortList + i)->number].groupNumber << endl;
-        cout << List[(SortList + i)->number].groupListNumber << endl;
-        cout << "Term grades:";
-        for (int j = 0; j < 8; j++) {
-            cout << List[(SortList + i)->number].gradesForTheLastSession[j] << " ";
-        }
-        cout << endl;
-        cout << List[(SortList + i)->number].formOfEducation << endl;
-        cout << ctime(&(List[(SortList + i)->number].dataChanges)) << endl;
-    }
-    std::cout << std::endl << std::endl;
-    free(SortList); // осовобождения памяти функции малок
-}
 
 int main()
 {
-    students* stu_1 = getAllDataFromFile();
-    printStudents(stu_1);
-
     //createStudents();
 
     //makingChangesToAnexiStingRecord();
    
     //outputAllStudentsGroupN();
 
+    students* stu_1 = getAllDataFromFile();
+    printStudents(stu_1);
 
     //memoryEdit(stu_1);
 
-
-    outputAllStudentsGroupN(stu_1);
-
-    //listTop(stu_1);
+    listStu(stu_1);
 
     cin.get();
 }
